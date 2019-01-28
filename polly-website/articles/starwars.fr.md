9487aa2cf1939e839e0749dedf5594318acce2ea
Buddhika Geasman <bgeasman@gmail.com>
2019-01-27 22:03:44 -0600

    wrote lambda function

--- polly-website/articles/starwars.fr.md
+++ polly-website/articles/starwars.fr.md
@@ -1,9 +1,3 @@
----
-title: "Starwars"
-date: 2019-01-21T21:54:12-06:00
-draft: false
-type: "post"
----
 > I want to come with you to Alderaan. There's nothing for me here now. I want to learn the ways of the Force and be a Jedi, like my father before me. I want to come with you to Alderaan. There's nothing for me here now. I want to learn the ways of the Force and be a Jedi, like my father before me.
 
 Ye-ha! The plans you refer to will soon be back in our hands. The Force is strong with this one. I have you now. Don't be too proud of this technological terror you've constructed. The ability to destroy a planet is insignificant next to the power of the Force.
@@ -11,7 +5,3 @@ Ye-ha! The plans you refer to will soon be back in our hands. The Force is stron
 Remember, a Jedi can feel the Force flowing through him. I need your help, Luke. She needs your help. I'm getting too old for this sort of thing. You mean it controls your actions? What!?
 
 Leave that to me. Send a distress signal, and inform the Senate that all on board were killed. What?! Partially, but it also obeys your commands. Still, she's got a lot of spirit. I don't know, what do you think?
-
-<audio src="starwars.fr.mp3" >
-
-<readfile file="/articles/starwars.fr.md" markdown="true">
--- polly_lambda/lambda_function.py
+++ polly_lambda/lambda_function.py
@@ -11,3 +11,36 @@ def lambda_handler(event, context):
     supported_languages = os.environ['supported_languages'].split('')
     default_language = os.environ['default_language']
     polly_bucket = os.environ['polly_bucket']
+
+    s3_bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+
+    try:
+        whole_filename, file_extension = os.path.splitext(key)
+        filename = whole_filename.split("/")[-1]
+        text= str(s3.Object(s3_bucket,key).get()['Body'].read(), 'utf-8')
+        #get the language file_name.lang.md or file_name.md
+        language = filename.split(".")[-1]
+        voice_language = language if language in supported_languages else default_language
+
+        voice_id = os.environ[voice_language]
+
+        response - polly.synthesize_speech(
+        OutputFormat = output,
+        Text=text,
+        VoiceId = voice_id
+        )
+
+        polly_title = "{}.{}".format(filename, output)
+
+        if "AudioStream" in response:
+            with closing(response['AudioStream']) as stream:
+                output_title = os.path.join('/tmp', polly_title)
+                with open(output_file, 'ab') as f:
+                    f.write(stream.read())
+
+        s3.Object(polly_bucket, polly_title).upload_file(os.path.join("/tmp", polly_title), ExtraArgs={'ACL':'public-read'})
+
+    except Exception as e:
+        print('Error getting object {} from object')
+        raise e
